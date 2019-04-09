614ddd2d0b89736973d70b210b180a7d520afd7c
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-04-09 17:19:55 -0400

    Update allowed hosts & settings

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = False
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^$', include('posts.urls')),
+    #url(r'^$', include('posts.urls')),
+    url(r'^', include('posts.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls'))
 ]
